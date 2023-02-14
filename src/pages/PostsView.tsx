6d5e313f231e95eaa454fdcbbc07dbf431918751
ChenWarren <72281162+ChenWarren@users.noreply.github.com>
2023-02-14 13:39:31 -0800

    update infinite scrolling

--- src/pages/PostsView.tsx
+++ src/pages/PostsView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
@@ -13,29 +13,72 @@ export interface Post {
 // Define the PostsView component
 const PostsView = () => {
     const [posts, setPosts] = useState<Post[]>([])
+    const [renderPosts, setRenderPosts] = useState<Post[]>([])
+    const [page, setPage] = useState(0)
+    const [hasMore, setHasMore] = useState(true)
+    const loader = useRef<HTMLDivElement>(null)
 
     // Use the useEffect hook to fetch the posts list from the API
     useEffect(()=> {
         axios.get('https://jsonplaceholder.typicode.com/posts')
         .then(response => {
             setPosts(response.data)
+            setRenderPosts(response.data.slice(0, 20))
         })
         .catch( error => {
             console.log(error.message)
         })
     }, [])
 
+    // Define the handleObserver function
+    const handleObserver = (entities: IntersectionObserverEntry[]) => {
+        const target = entities[0]
+        if ( target.isIntersecting ) {
+            if ( page != 0 && page * 20 > posts.length ) {
+                setHasMore(false)
+                return
+            }
+            setRenderPosts(posts.slice(0, (page + 1) * 20))
+            setPage(page + 1)
+        }
+    }
+
+
+    // Use the useEffect hook to load more posts when the user scrolls to the bottom of the page
+    useEffect(() => {
+        const options = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 1.0,
+        }
+
+        const observer = new IntersectionObserver(handleObserver, options)
+
+        if ( loader.current ) {
+            observer.observe(loader.current)
+        }
+
+        return () => {
+            if ( loader.current ) {
+                observer.disconnect()
+            }
+        }
+
+
+    }, [renderPosts])
+
     return (
         <>
             <h1>Posts</h1>
             <div>
                 {/* Map over the posts array and render a link for each*/}
-                { posts.map( post => (
-                    <h2 key={post.id}>
+                { renderPosts.map( post => (
+                    <h2 key={post.id} ref={loader}>
                         {/* Use the React Router Dom Link component to create a link to the post detail view */}
                         <Link to={`/post/${post.id}`}>{post.title}</Link> 
                     </h2>
                 ))}
+                {!hasMore && <div>--End--</div>}
             </div>
         </>
     )
